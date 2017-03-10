af7bc86403fe8fdf5b681cda4d8c8fd2d49f9fb3
MoogieMoo <yxia1@stuy.edu>
2017-03-09 23:48:41 -0500

    Update ExecTime.java

--- ExecTime.java
+++ ExecTime.java
@@ -3,13 +3,16 @@
    how long it takes for QuickSort to sort an int[]
    of size n.
    1) We first created four arrays of increasing size
-      (from 10 to 10,000).
+      (from 10 to 100,000).
    2) Then, we used the buildArray()helper method given
       to us in the QuickSort skeleton file we got from 
       the homework yesterday.
    3) Finally, we used a for loop to test QuickSort on
       each array 1,000 times, and found the average time
-      to sort an array of size[n].      
+      to sort an array of size[n]. 
+      *EXCEPTION* We only tested the arry with size 
+      100,000 one hundred times because it exceeded the data
+      limit bounds of an int.
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 //class ExecTime
@@ -45,7 +48,7 @@ public class ExecTime {
 	}
 
 	tenTime /= 1000; 
-	System.out.println("Time for sorting array of size ten: ");
+	System.out.println("Time for sorting array of size 10: ");
 	System.out.println(tenTime);
 
 	//timing how long it takes to QuickSort an array of size 100
@@ -81,10 +84,10 @@ public class ExecTime {
 
 	thouTime /= 1000; 
 
-	System.out.println("Time for sorting array of size 1000: ");
+	System.out.println("Time for sorting array of size 1,000: ");
 	System.out.println(thouTime);
 
-	//timing how long it takes to QuickSort an array of size 10,000
+	//timing how long it takes to QuickSort an array of size 100,000
 	int hunthouTime = 0;
 
 	for ( int x = 0; x < 100; x++ ) {
@@ -99,7 +102,7 @@ public class ExecTime {
 
 	hunthouTime /= 100; 
 
-	System.out.println("Time for sorting array of size 100000: ");
+	System.out.println("Time for sorting array of size 100,000: ");
 	System.out.println(hunthouTime);
     }
 }
