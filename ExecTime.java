de47fd7435d28d7d39fe2fec7a3ba3ecde52bfd9
MoogieMoo <yxia1@stuy.edu>
2017-03-09 23:25:25 -0500

    Update ExecTime.java

--- ExecTime.java
+++ ExecTime.java
@@ -1,18 +1,37 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   ExecTime is essentially a Driver file that times 
+   how long it takes for QuickSort to sort an int[]
+   of size n.
+   1) We first created four arrays of increasing size
+      (from 10 to 10,000).
+   2) Then, we used the buildArray()helper method given
+      to us in the QuickSort skeleton file we got from 
+      the homework yesterday.
+   3) Finally, we used a for loop to test QuickSort on
+      each array 1,000 times, and found the average time
+      to sort an array of size[n].      
+   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+//class ExecTime
 public class ExecTime {
     
     public static void main( String[] args ) {
 	int max = 10000;
-	
+
+	//create arrays of increasing size to use for testing
 	int[] ten;
 	int[] hun;
 	int[] thou;
 	int[] hunthou;
 
+	//use the method buildArray() to randomly populate
+	//previously created arrays
 	ten = QuickSort.buildArray(10, max);
 	hun = QuickSort.buildArray(100, max);
 	thou = QuickSort.buildArray(1000, max);
 	hunthou = QuickSort.buildArray(100000, max);
-	
+
+	//timing how long it takes to QuickSort an array of size 10
 	int tenTime = 0;
 
 	for ( int x = 0; x < 1000; x++ ) {
@@ -28,7 +47,8 @@ public class ExecTime {
 	tenTime /= 1000; 
 	System.out.println("Time for sorting array of size ten: ");
 	System.out.println(tenTime);
-	
+
+	//timing how long it takes to QuickSort an array of size 100
 	int hunTime = 0;
 
 	for ( int x = 0; x < 1000; x++ ) {
@@ -46,6 +66,7 @@ public class ExecTime {
 	System.out.println("Time for sorting array of size 100: ");
 	System.out.println(hunTime);
 
+	//timing how long it takes to QuickSort an array of size 1,000
 	int thouTime = 0;
 
 	for ( int x = 0; x < 1000; x++ ){
@@ -63,6 +84,7 @@ public class ExecTime {
 	System.out.println("Time for sorting array of size 1000: ");
 	System.out.println(thouTime);
 
+	//timing how long it takes to QuickSort an array of size 10,000
 	int hunthouTime = 0;
 
 	for ( int x = 0; x < 100; x++ ) {
